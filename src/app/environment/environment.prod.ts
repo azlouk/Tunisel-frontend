6f15f9f447cf8e3e45affb2298f3c0bea1c8e990
Azlouk Mohamed <mohamed.azlouk1@gmail.com>
2024-05-13 12:40:56 +0100

    modification rapport Analyse

--- src/app/environment/environment.prod.ts
+++ src/app/environment/environment.prod.ts
@@ -1,4 +1,4 @@
 export  const environment={
   production:true,
-  apiUrl:"http://localhost:8081"
+  apiUrl:"http://195.35.25.160:8081"
 }
--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -1,5 +1,5 @@
 
 export const environment = {
   production: false,
-  apiUrl:"http://localhost:8081"
+  apiUrl:"http://195.35.25.160:8081"
 };
